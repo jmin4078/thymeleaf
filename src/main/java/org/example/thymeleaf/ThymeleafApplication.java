e13b9e763eb9a0e20f9a25477f30731ba1336389
jmin4078 <wpalsrl8588@gmail.com>
2026-08-04 11:27:35 +0900

    Configure application profiles and properties

--- src/main/java/org/example/thymeleaf/ThymeleafApplication.java
+++ src/main/java/org/example/thymeleaf/ThymeleafApplication.java
@@ -2,8 +2,10 @@ package org.example.thymeleaf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ThymeleafApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/example/thymeleaf/controller/MainController.java
+++ src/main/java/org/example/thymeleaf/controller/MainController.java
@@ -1,14 +1,34 @@
 package org.example.thymeleaf.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.thymeleaf.config.AppProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping
+@RequiredArgsConstructor
 public class MainController {
+    private final AppProperties appProperties;
+//    private final String msg;
+//
+//    public MainController(
+//            AppProperties appProperties,
+//            @Value("${app.message}") String msg) {
+//        this.appProperties = appProperties;
+//        this.msg = msg;
+//    }
+
+    @Value("${app.message}")
+    private String msg; // 일종의 필드 주입
+
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("msg", appProperties.message());
+        model.addAttribute("msg2", msg);
         return "index";
     }
 }
